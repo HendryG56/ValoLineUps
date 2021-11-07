50a38e9de24b330a119eb117a82a556480e60efb
Hendry <hendryg56@gmail.com>
2021-11-07 15:20:13 +0700

    ganti jdi youtubeplay

--- app/src/main/java/hendryg56/com/valolineups/PlayVideoActivity.java
+++ app/src/main/java/hendryg56/com/valolineups/PlayVideoActivity.java
@@ -6,34 +6,45 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.widget.MediaController;
+import android.widget.Toast;
 import android.widget.VideoView;
+
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
 
 public class PlayVideoActivity extends AppCompatActivity {
 
-    VideoView videoView;
+    YouTubePlayerView ytvideo;
     String lineups_name;
     String videopath;
+    String api_key = "AIzaSyA0NP8gMBCqQ7oTEChkq9WuXC5fGlk0tHg";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_video);
 
-        videoView = findViewById(R.id.lineups_video_view);
+        ytvideo = findViewById(R.id.lineups_video_view);
 
         Intent intent = getIntent();
         lineups_name = intent.getStringExtra("LineUpsName");
 
-        if(lineups_name.equals("Sova A Main")){
-            videopath =  "android.resource://" + getPackageName() + "/" + R.raw.sova_1;
-        }
+        ytvideo.initialize(api_key, new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                if(lineups_name.equals("Sova A Main")){
+                    youTubePlayer.loadVideo("HzeK7g8cD0Y");
+                    youTubePlayer.play();
+                }
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+                Toast.makeText(PlayVideoActivity.this, "Video Fail to Play", Toast.LENGTH_SHORT).show();
+            }
+        });
 
 
-        Uri uri = Uri.parse(videopath);
-        videoView.setVideoURI(uri);
-
-        MediaController mediaController = new MediaController(this);
-        videoView.setMediaController(mediaController);
-        mediaController.setAnchorView(videoView);
     }
 }
